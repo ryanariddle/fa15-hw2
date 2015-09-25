f37e412f597e1067944567d870c5bf595ec44de9
ryanriddle@berkeley.edu <ryanriddle@berkeley.edu>
2015-09-25 16:28:24 -0700

    Q1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
 
   def stringify
     @text = "You are nothing!"
+    redirect_to "/age"
   end
 
   def age
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'age', to: 'pages#age'
+  get 'stringify', to: 'pages#stringify'
+  post 'stringify', to: 'pages#stringify'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
